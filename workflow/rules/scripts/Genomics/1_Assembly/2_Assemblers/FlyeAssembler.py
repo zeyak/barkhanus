606f0f505720d4f3952b12ea66b71f5df60ffeed
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-28 18:24:46 +0200

    dev

--- workflow/rules/scripts/Genomics/1_Assembly/2_Assemblers/FlyeAssembler.py
+++ workflow/rules/scripts/Genomics/1_Assembly/2_Assemblers/FlyeAssembler.py
@@ -5,5 +5,4 @@ out_dir = snakemake.output.out_dir
 genome_size = snakemake.params.genome_size
 threads = snakemake.params.threads
 
-#--nano-hq
-shell(f"""flye --nano-raw {input} --genome-size {genome_size} --threads {threads} --out-dir {out_dir}""")
+shell(f"""flye --nano-hq {input} --genome-size {genome_size} --threads {threads} --out-dir {out_dir}""")
