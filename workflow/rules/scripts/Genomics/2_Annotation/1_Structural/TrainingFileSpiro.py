3ab347caffc56b2045d342eb1f7a650d02f892f0
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-29 13:55:32 +0200

    dev

--- workflow/rules/scripts/Genomics/2_Annotation/1_Structural/TrainingFileSpiro.py
+++ workflow/rules/scripts/Genomics/2_Annotation/1_Structural/TrainingFileSpiro.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-df =pd.read_csv("resources/TrainGlimmerHMM/S_salmonicida/ncbi_dataset/data/GCA_000497125.2/genomic.gff",
-                sep='\t', header=None, comment='#')
+# Read the GFF file, ignoring comment lines starting with '#'
+df = pd.read_csv("resources/TrainGlimmerHMM/S_salmonicida/ncbi_dataset/data/GCA_000497125.2/genomic.gff",
+                 sep='\t', header=None, comment='#')
 
-#get exons on teh 2nd column
-#make df with 3 colummns: 0,2, 3, 4
+# Filter to get only exon rows
 df_exons = df[df[2] == "exon"]
+
+# Select columns 0, 3, and 4
 df_exons = df_exons.iloc[:, [0, 3, 4]]
 
 df_exons.to_csv("resources/TrainGlimmerHMM/spiro_exons.csv", sep='\t', header=False, index=False)
