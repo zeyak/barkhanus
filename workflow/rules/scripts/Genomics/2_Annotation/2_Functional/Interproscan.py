5db7ff18f0bb18956ba7a9518f888936829fd5d7
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-06-01 12:00:17 +0200

    dev

--- workflow/rules/scripts/Genomics/2_Annotation/2_Functional/Interproscan.py
+++ workflow/rules/scripts/Genomics/2_Annotation/2_Functional/Interproscan.py
@@ -7,3 +7,8 @@ out = snakemake.output.out
 #remove the stars representing the stop codons
 shell(f"""sed 's/*//' {proteome} > {proteome}_.faa""")
 shell(f"""interproscan.sh -i {proteome}_.faa -o {out} -f tsv -iprlookup -goterms --pathways -cpu {threads} """)
+
+"""
+/data/zeynep/interproscan-5.47-82.0/interproscan.sh -i results/Genomics/2_Annotation/1_Structural/glimmerhmm/flye/raw/genome.faa_.faa -o results/Genomics/2_Annotation/interproscan_results.tsv -f tsv -iprlookup -goterms --pathways -cpu 32
+
+"""
