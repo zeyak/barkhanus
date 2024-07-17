d52d1e51c31ecdaf651b8eec47dab4565b7fed85
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-07-17 15:13:37 +0200

    dev

--- workflow/rules/scripts/ComparativeGenomics/1_GenomeStructureLevel/Repeatmodeler.py
+++ workflow/rules/scripts/ComparativeGenomics/1_GenomeStructureLevel/Repeatmodeler.py
@@ -4,5 +4,5 @@ threads = snakemake.params.threads
 db_name = snakemake.params.db_name
 engine= snakemake.params.engine
 
-shell(f"RepeatModeler -database {db_name} -engine {engine} -pa {threads}")
+shell(f"RepeatModeler -database {db_name} -engine {engine} -threads {threads}")
 
