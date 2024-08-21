fc094d1230e2a210f1c656ca7283d02b663201c3
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-08-21 11:15:55 +0200

    dev

--- workflow/rules/scripts/ComparativeGenomics/2_SequenceSimilarityLevel/orthofinder.py
+++ workflow/rules/scripts/ComparativeGenomics/2_SequenceSimilarityLevel/orthofinder.py
@@ -5,4 +5,4 @@ proteome = snakemake.input.proteome
 out = snakemake.output[0]
 
 # -og : stop after inferring orthogroups
-shell(f"""orthofinder -f {proteome} -o {out} -og -S blast""")
+shell(f"""orthofinder -f {proteome} -og -S blast""")
